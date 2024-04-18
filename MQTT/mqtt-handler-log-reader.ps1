8e1cd2ef2707e7382247cc599e013b98747ad2a8
Mira Ojamo <1149108+Siikakala@users.noreply.github.com>
2024-04-18 23:22:03 +0300

    "oh right, get-content barfs if file doesn't exist. Let's add handling for that"

--- MQTT/mqtt-handler-log-reader.ps1
+++ MQTT/mqtt-handler-log-reader.ps1
@@ -3,13 +3,13 @@ $params = Import-PowerShellDataFile .\parameters.psd1
 $LogPath = (Get-Item $params.LogPath).Target[0]
 $StaticLogFile = $LogPath + "\" + $params.LogFilePrefix
 $ctrlc = $false
-while(-not $ctrlc){
+while (-not $ctrlc) {
     $date = Get-Date
     $reader = Start-Job -ScriptBlock {
-        param($FilePrefix,$DateSyntax)
+        param($FilePrefix, $DateSyntax)
         Get-Content "$FilePrefix$((Get-Date).tostring($DateSyntax)).log" -Wait
     } -ArgumentList $StaticLogFile, $params.LogFileDateSyntax
-    while((Get-Date).day -eq $date.day){
+    while ((Get-Date).day -eq $date.day) {
         if ([console]::KeyAvailable) {
             $key = [system.console]::readkey($true)
             if (($key.modifiers -band [consolemodifiers]"control") -and ($key.key -eq "C")) {
@@ -17,11 +17,21 @@ while(-not $ctrlc){
                 break
             }
         }
+        if ($reader.State -ne "Running") {
+            Write-Host "Reader died, waiting 30 seconds before reload"
+            Start-Sleep -Seconds 30
+            break
+        }
         Receive-Job $reader
         Start-Sleep -Milliseconds 50
     }
     $reader | Stop-Job | Remove-Job
-    if(-not $ctrlc){
-        Write-Host "Day changed, reloading reader"
+    if (-not $ctrlc) {
+        if ((Get-Date).day -eq $date.day) {
+            Write-Host "Reloading reader"
+        }
+        else {
+            Write-Host "Day changed, reloading reader"
+        }
     }
 }
