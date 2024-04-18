729bea1d3c09bdfea8dbfcc9775f4862b87ee750
Mira Ojamo <1149108+Siikakala@users.noreply.github.com>
2024-04-18 17:58:36 +0300

    Quick-n-dirty log reader for the handler with minimal hard-coding

---
+++ MQTT/mqtt-handler-log-reader.ps1
@@ -0,0 +1,25 @@
+[console]::TreatControlCAsInput = $true
+$params = Import-PowerShellDataFile .\parameters.psd1
+$LogPath = (Get-Item $params.LogPath).Target[0]
+$StaticLogFile = $LogPath + "\" + $params.LogFilePrefix
+$ctrlc = $false
+while(-not $ctrlc){
+    $date = Get-Date
+    $reader = Start-Job -ScriptBlock {
+        param($FilePrefix,$DateSyntax)
+        Get-Content "$FilePrefix$((Get-Date).tostring($DateSyntax)).log" -Wait
+    } -ArgumentList $StaticLogFile, $params.LogFileDateSyntax
+    while((Get-Date).day -eq $date.day){
+        if ([console]::KeyAvailable) {
+            $key = [system.console]::readkey($true)
+            if (($key.modifiers -band [consolemodifiers]"control") -and ($key.key -eq "C")) {
+                $ctrlc = $true
+                break
+            }
+        }
+        Receive-Job $reader
+        Start-Sleep -Milliseconds 50
+    }
+    $reader | Stop-Job | Remove-Job
+    Write-Host "Day changed, reloading reader"
+}
